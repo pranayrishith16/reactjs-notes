6e4b4779bdb798f9115d2bdb6d5e1baa5ae86d64
pranay <pranayrishith007@gmail.com>
2022-03-02 10:41:48 +0530

    next

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import { useState } from 'react';
+import {nanoid} from 'nanoid'
 import './index.css';
 import NoteList from './components/NotesList';
 
 function App() {
-  const [notes, setnotes] = useState([])
+  const [notes, setnotes] = useState([{
+    id:nanoid(),
+    text:"This is my first note",
+    date:"15/04/2022"
+  },
+  {
+    id:nanoid(),
+    text:"This is my second note",
+    date:"16/04/2022"
+  },
+  {
+    id:nanoid(),
+    text:"This is my third note",
+    date:"17/04/2022"
+  },
+  {
+    id:nanoid(),
+    text:"This is my fourth note",
+    date:"18/04/2022"
+  }]);
+  console.log(notes);
   return (
     <div className="container">
-      <NoteList/>
+      <NoteList notes={notes}/>
     </div>
   );
 }
